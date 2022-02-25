23eb49fe2e92bdda01d9876f7b562571ec81ea14
Christopher Woodall <chris.j.woodall@gmail.com>
2022-02-24 22:06:57 -0500

    Initial implementation of control variables using an interactive matplotlib plot

--- autowah/console.py
+++ autowah/console.py
@@ -1,17 +1,20 @@
-from typing import Dict
+from dataclasses import dataclass
+from functools import partial
+from typing import Any, Dict, List, Optional, Tuple
 import pyaudio
 import numpy as np
 import time
+from autowah.control_values import ControlValueDef, ControlValues
+
 from numpy_ringbuffer import RingBuffer
 
-import wave
-from multiprocessing import Process, Queue
+from multiprocessing import Process, Queue, Value, Lock
 
 from .envelope_follower import EnvelopeFollower
 from .variable_cutoff_filter import VariableCutoffFilter
 from .variable_cutoff_biquad_filter import VariableCutoffBiquadFilter
 import matplotlib.pyplot as plt
-
+from matplotlib.widgets import Slider, Button, CheckButtons
 
 # https://www.geeksforgeeks.org/check-data-type-in-numpy/
 # https://stackoverflow.com/questions/56147161/python-matplotlib-update-plot-in-the-background
@@ -26,7 +29,7 @@ CHUNK = int(1024/2)
 HISTORY_LENGTH = CHUNK * 20
 
 
-def plotter(scope: Dict):
+def plotter(scope: Dict, cv):
     scope_buffers = {
         k: RingBuffer(capacity=HISTORY_LENGTH) for k in scope.keys()
     }
@@ -36,9 +39,9 @@ def plotter(scope: Dict):
     plt.style.use("ggplot")
 
     fig = plt.figure()
-    ax1: plt.Axes = fig.add_subplot(311)
-    ax2 = fig.add_subplot(312, sharex=ax1)
-    ax3 = fig.add_subplot(313, sharex=ax1)
+    ax1: plt.Axes = fig.add_subplot(411)
+    ax2 = fig.add_subplot(412, sharex=ax1)
+    ax3 = fig.add_subplot(413, sharex=ax1)
     ax1.set_ylim((-1, 1))
     ax2.set_ylim((-1, 1))
     ax3.set_ylim((-1, 1))
@@ -52,6 +55,37 @@ def plotter(scope: Dict):
     for name, q in scope.items():
         while not q.empty():
             scope_buffers[name].extend(q.get_nowait())
+    
+    def update_value(cv_name: str, val):
+        "test"
+        cv.values[cv_name].value = val
+        fig.canvas.draw_idle()
+    
+    def toggle_value(cv_name: str, __name):
+        cv.values[cv_name].value = not cv.values[cv_name].value
+
+    slider_axes = []
+    sliders = []
+    for i, x in enumerate(cv.values.values()):
+        if x.typestr == 'b':
+            # Make checkbuttons with all plotted lines with correct visibility
+            rax = plt.axes([0.25, i*0.03+.01, 0.25, 0.03])
+            # labels = [str(line.get_label()) for line in lines]
+            # visibility = [line.get_visible() for line in lines]
+            widget = CheckButtons(rax, [x.name], [x.value])
+            widget.on_clicked(partial(toggle_value, x.name))
+            sliders.append(widget)
+        else:
+            slider_axes.append(plt.axes([0.25, i*0.03+.01, 0.65, 0.03]))
+            slider = Slider(
+                ax=slider_axes[-1],
+                label=x.name,
+                valmin=x.min,
+                valmax=x.max,
+                valinit=x.value,
+            )
+            slider.on_changed(partial(update_value, x.name))
+            sliders.append(slider)
 
     while True:
         for name, q in scope.items():
@@ -65,31 +99,40 @@ def plotter(scope: Dict):
         fig.canvas.flush_events()
         fig.canvas.draw()
 
-def stream(scope):
+
+def stream(scope, cv):
     p = pyaudio.PyAudio()
 
     # Make these variables controlable
-    ENVELOPE_FOLLOWER_FC = 20
+    ENVELOPE_FOLLOWER_FC = 30
     envelope_follower = EnvelopeFollower(ENVELOPE_FOLLOWER_FC, RATE)
 
-    # Q!
-    # if filter_type is "fir":
-    #     lpf = VariableCutoffFilter(filter_len=31, fs=RATE, chunk=CHUNK)
-    # else:
-    lpf = VariableCutoffBiquadFilter(fs=RATE, chunk=CHUNK, Q=8)
+    with cv.lock:
+        lpf = VariableCutoffBiquadFilter(fs=RATE, chunk=CHUNK)
     
-    starting_freq = 10
-    sensitivity = 10000
 
     def callback(in_data, frame_count, time_info, flag):
+        lpf.Q = cv.values['Q'].value
+        starting_freq = cv.values['starting_freq'].value
+        sensitivity = cv.values['sensitivity'].value
+        is_bandpass = cv.values['is_bandpass'].value
+        if is_bandpass:
+            lpf.filter_type = 'bandpass'
+        else:
+            lpf.filter_type = 'low'
+        gain = cv.values['gain'].value
+        mix = cv.values['mix'].value
+        envelope_gain = cv.values['envelope_gain'].value
+
         # using Numpy to convert to array for processing
         audio_data = np.fromstring(in_data, dtype=np.float32)
+
         # Process data here
-
-        envelope = envelope_follower.run(audio_data)*1
+        envelope = envelope_follower.run(audio_data)*envelope_gain
         freqs = starting_freq + envelope * sensitivity
+        freqs = np.clip(freqs, .001, RATE/2-.1)
 
-        out = .8* lpf.run(audio_data, freqs)
+        out = mix*gain* lpf.run(audio_data, freqs) + audio_data*(1-mix)
         out = out.astype(np.float32)
 
         scope["in"].put_nowait(audio_data)
@@ -116,14 +159,28 @@ def stream(scope):
 
     p.terminate()
 
+
+
 def run():
     scope = {
         "in": Queue(),
         "envelope": Queue(),
         "out": Queue(),
     }
-    stream_proc = Process(target=stream, args=(scope,))
-    plotter_proc = Process(target=plotter, args=(scope,))
+
+    control_values = ControlValues(
+        [
+            ControlValueDef('is_bandpass', typestr = 'b', init_value = False),
+            ControlValueDef('starting_freq', 'f', 100, 10, RATE/2 - .1),
+            ControlValueDef('Q', 'f', 8.0, .1, 20),
+            ControlValueDef('sensitivity', 'f', init_value = RATE/4, min=0, max=RATE/2),
+            ControlValueDef('gain', 'f', init_value = .8),
+            ControlValueDef('mix', 'f', init_value = .8),
+            ControlValueDef('envelope_gain', 'f', init_value = 1, min=0, max=4),
+        ])
+
+    stream_proc = Process(target=stream, args=(scope,control_values,))
+    plotter_proc = Process(target=plotter, args=(scope,control_values,))
     plotter_proc.start()
     stream_proc.start()
 
---
+++ autowah/control_values.py
@@ -0,0 +1,57 @@
+
+
+from dataclasses import dataclass
+from multiprocessing import Lock, Value
+from typing import Any, List
+
+
+@dataclass
+class ControlValueDef():
+    name: str
+    typestr: str = 'f'
+    init_value: Any = 0.0
+    min: float = 0
+    max: float = 1
+
+class ControlValue():
+    def __init__(self, cvd: ControlValueDef):
+        self.lock = Lock()
+        self.defn = cvd
+        self.val = Value(cvd.typestr, cvd.init_value)
+
+    @property
+    def value(self):
+        with self.lock:
+            return self.val.value
+
+    @value.setter
+    def value(self, val):
+        with self.lock:
+            self.val.value = val
+
+    @property
+    def name(self):
+        return self.defn.name
+
+    @property
+    def typestr(self):
+        return self.defn.typestr
+
+    @property
+    def init_value(self):
+        return self.defn.init_value
+
+    @property
+    def min(self):
+        return self.defn.min
+
+    @property
+    def max(self):
+        return self.defn.max
+
+class ControlValues():
+    def __init__(self, values: List[ControlValueDef]):
+        self.lock = Lock()
+        self.values = {
+            x.name: ControlValue(x) for x in values
+        }
